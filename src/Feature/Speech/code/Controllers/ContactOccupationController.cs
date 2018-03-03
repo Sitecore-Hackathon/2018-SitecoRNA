5a322afcb0eb1c09de410d41812142ec50910821
Zachary_Kniebel <Zachary_Kniebel@epam.com>
2018-03-03 16:24:49 -0500

    Adds comments and todos, installed webapi.core and added the logic to retrieve the data from the request object

--- src/Feature/Speech/code/Controllers/ContactOccupationController.cs
+++ src/Feature/Speech/code/Controllers/ContactOccupationController.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace Rna.Feature.Speech.Controllers
 {
     public class ContactOccupationController : Controller
     {
-        [HttpPost]
-        public ActionResult AddOrUpdateContactOccupation()
+        /// <summary>
+        /// Adds a new or updates the existing contact with the given email and occupation (discerned from the passed audio file)
+        /// </summary>
+        /// <param name="email">The email of the contact to be used as an identifier</param>
+        /// <remarks>
+        /// Expects an auto file named "file" to be included in the post
+        /// </remarks>
+        /// <returns>{ Success: true }, if successful</returns>
+        [System.Web.Mvc.HttpPost]
+        public ActionResult AddOrUpdateContactOccupation([FromBody] string email)
         {
-            var test = "hello";
+            // TODO: null-checking and error handling
+
+            // get the file from the request
+            var file = Request.Files["file"];
+
+            // TODO: speech to text
+            
+            // TODO: call Varun's stuff
 
             return new JsonResult()
             {
