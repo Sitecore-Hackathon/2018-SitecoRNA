c2537c2d6bfac8ba6054fc9c4e9c3b6432b2a285
Zachary_Kniebel <Zachary_Kniebel@epam.com>
2018-03-03 17:55:10 -0500

    Added in logic for the speech to text service call and updated the audio file to be a wav (only file type supported by MS right now)

--- src/Feature/Speech/code/Controllers/ContactOccupationController.cs
+++ src/Feature/Speech/code/Controllers/ContactOccupationController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -20,14 +22,84 @@ namespace Rna.Feature.Speech.Controllers
         [System.Web.Mvc.HttpPost]
         public ActionResult AddOrUpdateContactOccupation([FromBody] string email)
         {
-            // TODO: null-checking and error handling
+            // TODO: null-checking and error handling throughout this method
+            // TODO: move logic out of controller and implement a CQRS pattern
 
             // get the file from the request
             var file = Request.Files["file"];
+            if (file == null)
+            {
+                return new JsonResult()
+                {
+                    Data = new { Success = false, ErrorMessage = "File was null" }
+                };
+            }
 
-            // TODO: speech to text
+            // get the service uri and subscription key
+            var serviceUri = Sitecore.Configuration.Settings.GetSetting("Microsoft.API.Speech.ServiceUri");
+            var subscriptionKey = Sitecore.Configuration.Settings.GetSetting("Microsoft.API.Speech.SubscriptionKey");
+
+            var request = (HttpWebRequest)WebRequest.Create(serviceUri);
+            request.SendChunked = false;
+            request.Accept = @"application/json;text/xml";
+            request.Method = "POST";
+            request.ProtocolVersion = HttpVersion.Version11;
+            request.ContentType = @"audio/wav; codec=audio/pcm; samplerate=16000";
+            request.Headers["Ocp-Apim-Subscription-Key"] = subscriptionKey;
             
+            using (var stream = file.InputStream)
+            {
+                using (var requestStream = request.GetRequestStream())
+                {
+                    var buffer = new byte[checked((uint)stream.Length)];
+                    var bytesRead = 0;
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        requestStream.Write(buffer, 0, bytesRead);
+                    }
+
+                    // Flush
+                    requestStream.Flush();
+                }
+            }
+
+            string responseString = null;
+            using (var response = request.GetResponse())
+            {
+                var statusCode = ((HttpWebResponse) response).StatusCode;
+                if (statusCode != HttpStatusCode.OK)
+                {
+                    return new JsonResult()
+                    {
+                        Data = new { Success = false, ErrorMessage = $"Service call to Microsoft Speech API failed. Response status: {statusCode}" }
+                    };
+                }
+
+                var responseStream = response.GetResponseStream();
+                if (responseStream == null)
+                {
+                    return new JsonResult()
+                    {
+                        Data = new { Success = false, ErrorMessage = $"Service call to Microsoft Speech API failed. Response stream was null" }
+                    };
+                }
+
+                using (var sr = new StreamReader(responseStream))
+                {
+                    responseString = sr.ReadToEnd();
+                }
+            }
+
+            if (string.IsNullOrEmpty(responseString))
+            {
+                return new JsonResult()
+                {
+                    Data = new { Success = false, ErrorMessage = $"Service call to Microsoft Speech API failed. Response string was null or empty" }
+                };
+            }
             // TODO: call Varun's stuff
+
+
 
             return new JsonResult()
             {
