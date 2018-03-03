b8e393f019527e4cc62edb996c8ed33d18735e7f
Zachary_Kniebel <Zachary_Kniebel@epam.com>
2018-03-03 15:39:09 -0500

    Adds the new Speech feature module and the speech service for the demo to communicate with

---
+++ src/Feature/Speech/code/Controllers/ContactOccupationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rna.Feature.Speech.Controllers
+{
+    public class ContactOccupationController : Controller
+    {
+        [HttpPost]
+        public ActionResult AddOrUpdateContactOccupation()
+        {
+            var test = "hello";
+
+            return new JsonResult()
+            {
+                Data = new { Success= true}
+            };
+        }
+    }
+}
